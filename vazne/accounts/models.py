46ccf32ff252edbfa8b181555b2bb4e12be10211
Zahra-Jiriaei <zahra.jiriaeii@gmail.com>
2022-11-08 18:58:14 +0330

    Add User model

--- vazne/accounts/models.py
+++ vazne/accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+    role_choice=(("Customer"),("Coach"))
+    role = models.CharField(max_length=1, choices=role_choice)
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
