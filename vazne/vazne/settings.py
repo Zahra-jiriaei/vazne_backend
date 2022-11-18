7fc0c70e9a787a3722a0e78146081281cfcefc7b
Zahra-Jiriaei <zahra.jiriaeii@gmail.com>
2022-11-18 11:49:25 +0330

    add mysql

--- vazne/vazne/settings.py
+++ vazne/vazne/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'vazne.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vazne',
+        'USER': 'admin',
+        'PASSWORD': '123456789',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
