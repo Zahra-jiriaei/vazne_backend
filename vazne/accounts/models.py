3135c55dbc4db21ce06f94e4442b1e8987e1e547
AmirhosseinSa <101449829+AmirhosseinSa@users.noreply.github.com>
2022-12-14 21:53:32 +0330

    Update models.py

--- vazne/accounts/models.py
+++ vazne/accounts/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext as _
 from .managers import CustomUserManager
 
 class CustomUser(User):
-    role_choice=(("Customer"),("Coach"))
+    role_choice=((0,"Customer"),(1,"Coach"))
     role = models.CharField(max_length=1, choices=role_choice)
     objects = CustomUserManager()
 
